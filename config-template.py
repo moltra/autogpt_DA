32be6ae9e0dc29da915508aefc7bebbf6dd8e10e
Moltra <smsmail@roadrunner.com>
2023-04-27 12:05:20 -0400

    added config_template.py
    Added template file for config.py so it is easier to know what to add to
    use the script.

---
+++ config-template.py
@@ -0,0 +1,7 @@
+# storing API data
+#api_key = "[YOUR API KEY]"
+#api_secret = "[YOUR API SECRET]"
+#access_token = "[YOUR API ACCESS TOKEN]"
+#token_secret = "[YOUR API TOKEN SECRET]"
+
+github_token_value="Enter_your_github_token"
